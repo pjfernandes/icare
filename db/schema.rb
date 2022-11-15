898f957d9c043f3241d92ce03a6bb9f13b6bcc4e
Pedro Fernandes <pjf_fernandes@yahoo.com.br>
2022-11-15 12:35:38 -0300

    login page added

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_12_021823) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_15_145006) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "caregivers", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_caregivers_on_user_id"
+  end
+
+  create_table "clients", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_clients_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,8 +41,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_12_021823) do
     t.string "type"
     t.string "address"
     t.string "city"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "caregivers", "users"
+  add_foreign_key "clients", "users"
 end
